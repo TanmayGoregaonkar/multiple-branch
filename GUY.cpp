d2de0adcf0c4e33f4da6e5f8d5b81599c0744082
TanmayGoregaonkar <93464019+TanmayGoregaonkar@users.noreply.github.com>
2023-01-07 21:33:07 +0530

    pseudo

---
+++ GUY.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<(n-1)/2<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
